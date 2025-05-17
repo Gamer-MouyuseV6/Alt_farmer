2f9d1f4e6079ad03eb138bff27277133df0e6298
Death Hub <sunkistsugar2@gmail.com>
2025-05-17 20:13:37 +1000

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+_G.Config = {
+    Auto_farm = true,
+    Auto_respawn = true,
+    Auto_Fast_respawn = true,
+    MainAccount = "Usernamehere",
+    Auto_Attack = true
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamer-MouyuseV6/Alt_farmer/refs/heads/main/Death_Hub.lua"))()
